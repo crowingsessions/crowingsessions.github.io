7ec510e742dc7a1ff286adae043b36a602cd3b15
crowingsessions <111751343+crowingsessions@users.noreply.github.com>
2023-02-27 13:58:05 +0000

    Update session 2023-02-22 video and presentation
    Add with youtube video and celso presentation

--- data/sessions_logs.js
+++ data/sessions_logs.js
@@ -20,7 +20,12 @@ var sessionsLogs = [
         "type": "url"
       },
       {
-        "url": "https://www.youtube.com/watch?v=",
+        "url": "https://docs.google.com/presentation/d/1NNfZlOaEe6gAkyHk58ZryQxWhN0xz9Vf/edit?usp=sharing&ouid=116640801668531279977&rtpof=true&sd=true",
+        "name": "Celso's Presentation slides",
+        "type": "url"
+      },
+      {
+        "url": "https://www.youtube.com/watch?v=6brzmeFyBVo",
         "name": "Check session",
         "type": "youtube"
       },
